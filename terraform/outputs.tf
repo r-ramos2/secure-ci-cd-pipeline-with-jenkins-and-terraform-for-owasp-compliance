4e49a8f4189514acff7c3ff555941d49ef4b73bc
R <90172092+r-ramos2@users.noreply.github.com>
2025-06-17 13:10:49 -0600

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,29 @@
+// outputs.tf
+#────────────────────────────────────────────────────────────────
+# Outputs for user convenience
+#────────────────────────────────────────────────────────────────
+
+output "private_key_path" {
+  description = "Path to the generated SSH private key"
+  value       = local_file.private_key_pem.filename
+}
+
 output "instance_public_ip" {
-  description = "Public IP address of the Jenkins EC2 instance"
-  value       = aws_instance.jenkins_instance.public_ip
+  description = "Public IP of Jenkins EC2"
+  value       = aws_instance.jenkins.public_ip
 }
 
 output "jenkins_url" {
   description = "Jenkins access URL"
-  value       = "http://${aws_instance.jenkins_instance.public_ip}:8080"
+  value       = "http://${aws_instance.jenkins.public_ip}:8080"
 }
 
 output "sonarqube_url" {
   description = "SonarQube access URL"
-  value       = "http://${aws_instance.jenkins_instance.public_ip}:9000"
+  value       = "http://${aws_instance.jenkins.public_ip}:9000"
 }
 
 output "react_app_url" {
   description = "React app access URL"
-  value       = "http://${aws_instance.jenkins_instance.public_ip}:3000"
+  value       = "http://${aws_instance.jenkins.public_ip}:3000"
 }
