9f8cf9f2c95138e0cc4fd593c3d805b669b55982
R <90172092+r-ramos2@users.noreply.github.com>
2025-06-20 11:35:32 -0600

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,6 +4,21 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+variable "vpc_cidr_block" {
+  description = "CIDR block for the primary VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+variable "availability_zone" {
+  description = "AZ in which to create the public subnet"
+  type        = string
+  default     = "us-east-1a"
+}
 variable "ami_name_filter" {
   description = "Filter to find the latest Amazon Linux 2 AMI"
   type        = string
@@ -14,8 +29,13 @@ variable "instance_type" {
   type        = string
   default     = "t2.large"
 }
+variable "root_volume_size" {
+  description = "Root EBS volume size for Jenkins (GB)"
+  type        = number
+  default     = 30
+}
 variable "my_ip" {
   description = "Your IP CIDR block for SSH access"
   type        = string
-  default     = "203.0.113.0/32" // change to your own private IP address
+  default     = "203.0.113.0/32"
 }
