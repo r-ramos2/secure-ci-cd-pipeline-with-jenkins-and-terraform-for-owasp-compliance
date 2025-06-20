78cc9b4589dc9d1d80102fe9264cd829e9b1921b
R <90172092+r-ramos2@users.noreply.github.com>
2025-06-20 11:57:39 -0600

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,13 +2,12 @@
 terraform {
   required_version = ">= 1.5.0"
   required_providers {
-    aws    = { source = "hashicorp/aws",    version = ">= 4.0" }
+    aws    = { source = "hashicorp/aws", version = ">= 4.0" }
     tls    = { source = "hashicorp/tls" }
     random = { source = "hashicorp/random" }
     local  = { source = "hashicorp/local" }
   }
 }
 provider "aws" {
-  # AWS region for resources
   region = var.region
 }
